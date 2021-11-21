ed863151f6c39750a318fd5d56b489475a704344
Vlad Kozlovskiy <kozlovskii9928@gmail.com>
2021-11-21 12:02:49 +0300

    First commit

---
+++ NewYearGift/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewYearGift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("когда пойдем пить!");
+        }
+    }
+}
